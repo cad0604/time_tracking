8aa30df3d7eed4e59d1ffc3c8c0f584f304aad3b
cad0604 <cad910604@gmail.com>
2024-10-25 01:43:51 +0700

    update url and creating new db and seed data

--- api/index.js
+++ api/index.js
@@ -8,7 +8,6 @@ const { SwaggerSpecs, SwaggerOptions } = require("../app/utils/common/swagger");
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const { createDatabase } = require("../app/utils/db/connector");
 app.set("trust proxy", true);
 app.use(express.static(path.join(process.cwd(), "/public")));
 app.use(express.static(path.join(process.cwd(), "frontend", "build")));
@@ -68,33 +67,6 @@ const syncOptions = {
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}.`);
 // });
-const dropAllTables = async () => {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-
-  const QUERY = `select TABLE_NAME from information_schema.tables where table_schema = '${process.env.DB_NAME}'`;
-  try {
-    const [results] = await connection.query(QUERY);
-
-    const fdSQL = "SET foreign_key_checks = 0;";
-    await connection.query(fdSQL);
-    results.map(async (res) => {
-      const SQL = `drop table ${res.TABLE_NAME};`;
-      await connection.query(SQL);
-    });
-
-    const feSQL = "SET foreign_key_checks = 1;";
-    await connection.query(feSQL);
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-
-dropAllTables();
 
 modelsFolderPaths.map((modelsFolderPath) => {
   const dir = path.join(__dirname, modelsFolderPath);
