ea928037d08ad258395f35a543208d381b31462c
cad0604 <cad910604@gmail.com>
2024-10-25 01:40:22 +0700

    to delete all tables in previous db

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const path = require("path");
 const swaggerUI = require("swagger-ui-express");
 require("dotenv").config();
 const { SwaggerSpecs, SwaggerOptions } = require("../app/utils/common/swagger");
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const { createDatabase } = require("../app/utils/db/connector");
 app.set("trust proxy", true);
@@ -41,7 +43,7 @@ const modelsFolderPaths = [
 const db = { sequelize, Sequelize, models: {} };
 const { seedDatabase } = require("../app/utils/db/seed");
 
-createDatabase();
+// createDatabase();
 require("../app/routes/index")(app);
 
 app.use((req, res, next) => {
@@ -66,6 +68,33 @@ const syncOptions = {
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}.`);
 // });
+const dropAllTables = async () => {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  const QUERY = `select TABLE_NAME from information_schema.tables where table_schema = '${process.env.DB_NAME}'`;
+  try {
+    const [results] = await connection.query(QUERY);
+
+    const fdSQL = "SET foreign_key_checks = 0;";
+    await connection.query(fdSQL);
+    results.map(async (res) => {
+      const SQL = `drop table ${res.TABLE_NAME};`;
+      await connection.query(SQL);
+    });
+
+    const feSQL = "SET foreign_key_checks = 1;";
+    await connection.query(feSQL);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+dropAllTables();
 
 modelsFolderPaths.map((modelsFolderPath) => {
   const dir = path.join(__dirname, modelsFolderPath);
